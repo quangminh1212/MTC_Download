161f63b17181d97ded5aa8932935c415caa7f78e
Bach Minh Quang <0947556012abc@gmail.com>
2026-06-15 02:41:23 +0700

    Revert "Merge branch 'main' of https://github.com/quangminh1212/MTC_Download"
    This reverts commit cb0c006b9b6707f7b9bee376b26c2b49e0eb0422, reversing
    changes made to 01ecc8fe0a22820e198cde9abe2fb4a511fd0262.

--- git_probe.py
+++
@@ -1,8 +0,0 @@
-import subprocess, os, json
-repo=r'C:\Dev\MTC'
-cmd=['git','status','--porcelain=v1','-uall','--']
-r=subprocess.run(cmd,cwd=repo,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-print('return',r.returncode)
-print('stdout bytes',len(r.stdout))
-print(r.stdout[:500].decode('utf-8','replace'))
-print('stderr',r.stderr.decode('utf-8','replace'))
